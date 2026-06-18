a7b08f22147e017ebf882b597116d4ea484ac65e
Bruna561 <bruna.nobrega@nave.org.br>
2026-06-18 11:07:19 -0300

    Update Atv03RespostaErrada.js

--- src/Atv03RespostaErrada.js
+++ src/Atv03RespostaErrada.js
@@ -18,3 +18,56 @@
   * Ao final deste arquivo, use "export default app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('indexAtv.html', { root: './src' });
+});
+
+app.get('/cadastro', (req, res) => {
+  const { nome, email, controle, personagem } = req.query;
+
+  const personagens = [].concat(personagem || []);
+
+  if (personagens.length > 5) {
+    return res.send('Quantidade de personagens escolhida superior ao necessário (5)');
+  }
+
+  if (personagens.length < 5) {
+    return res.send('Quantidade de personagens escolhida inferior ao necessário (5)');
+  }
+
+  res.send(`
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+
+  <p><strong>Participante:</strong> ${nome}</p>
+  <p><strong>Email:</strong> ${email}</p>
+  <p><strong>Tipo de controle:</strong> ${controle}</p>
+
+  <p><strong>Personagens escolhidos:</strong></p>
+
+  <p>${personagens[0]}</p>
+  <p>${personagens[1]}</p>
+  <p>${personagens[2]}</p>
+  <p>${personagens[3]}</p>
+  <p>${personagens[4]}</p>
+</body>
+</html>
+  `);
+});
+
+export default app;
