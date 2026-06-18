6cd86658615ab2f507a98bfccdc77252dc65bc10
joao <joao.philippe@nave.org.br>
2026-06-18 11:09:48 -0300

    Update Atv01RespostaSimples.js

--- src/Atv01RespostaSimples.js
+++ src/Atv01RespostaSimples.js
@@ -27,3 +27,72 @@
   * Ao final deste arquivo, use "export default app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+
+/*
+  * [ATIVIDADE 1 - Resposta simples]
+  *
+  * Crie um aplicativo Express com duas rotas do tipo "GET".
+  * A primeira rota deve ser para "/", e ao acessar ela,
+  * o arquivo "indexAtv.html" deve ser enviado.
+  *
+  * A segunda rota deve ter o endereço "/cadastro".
+  * Ela é acessada quando o formulário do "indexAtv.html"
+  * é enviado. Quando esta rota for acessada, ela deve
+  * retornar a resposta abaixo:
+  *
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+</body>
+</html>
+  *
+  * Ao final deste arquivo, use "export default app" para
+  * exportar o objeto do servidor para os testes automatizados.
+  */
+
+
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+
+const porta = 3000
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+  console.log(req)
+  res.send(`<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+</body>
+</html>`)
+})
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
