2f70d6ebb0dce305492f8aa79460ea196fcb4fda
mavimdddu <mariavitoria.matos@nave.org.br>
2026-07-08 10:29:45 -0300

    Update Atv04RespostaStatus.js

--- src/Atv04RespostaStatus.js
+++ src/Atv04RespostaStatus.js
@@ -1,25 +1,70 @@
 /*
-  * [ATIVIDADE 3 - Resposta status]
+  * [ATIVIDADE 1 - Resposta simples]
   *
-  * Para esta questão, copie o que foi feito na atividade
-  * anterior e realize a modificação abaixo:
+  * Crie um aplicativo Express com duas rotas do tipo "GET".
+  * A primeira rota deve ser para "/", e ao acessar ela,
+  * o arquivo "indexAtv.html" deve ser enviado.
   *
-  * Modifique o comportamento da resposta de erro da
-  * rota "/cadastro" para que caso o usuário preencha mais
-  * ou menos que cinco personagens, seja enviada uma mensagem
-  * de erro com o status 422, que corresponde a requisição no
-  * formato correto e com informações válidas, mas o servidor
-  * não pode processar as informações.
+  * A segunda rota deve ter o endereço "/cadastro".
+  * Ela é acessada quando o formulário do "indexAtv.html"
+  * é enviado. Quando esta rota for acessada, ela deve
+  * retornar a resposta abaixo:
   *
-  * Na mensagem de erro da resposta, informe um dos dois
-  * textos abaixo:
-  * 
-  * Para mais que 5 personagens:
-  * "Quantidade de personagens escolhida superior ao necessário (5)"
-  *
-  * Para menos que 5 personagens:
-  * "Quantidade de personagens escolhida inferior ao necessário (5)"
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+</body>
+</html>
   *
   * Ao final deste arquivo, use "export default app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require('express');
+const path = require('path')
+const cors = require('cors')
+const app = express();
+const porta = 3000;
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+
+  if(req.query) {
+    res.send(`
+        <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="color-scheme" content="light dark"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+      <title>Prazer em lhe conhecer</title>
+    </head>
+    <body>
+      <h1>Cadastro feito com sucesso!</h1>
+    </body>
+    </html>
+    `);
+  }
+    
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
+
+module.exports = app
