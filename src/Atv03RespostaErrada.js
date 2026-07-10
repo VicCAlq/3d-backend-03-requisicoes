35adaad7ad3353ee0a8605769b8efb5d8d9b1c4f
damascenaa <jose.goncalves@nave.org.br>
2026-07-10 12:09:47 -0300

    Update Atv03RespostaErrada.js

--- src/Atv03RespostaErrada.js
+++ src/Atv03RespostaErrada.js
@@ -18,3 +18,61 @@
   * Ao final deste arquivo, use "export default app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require('express');
+const path = require('path')
+const cors = require('cors')
+const app = express();
+const porta = 3000;
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+  if(req.query) {
+     const { nome, email, controle, personagem } = req.query
+
+     if (typeof(personagem) === "string") {
+       res.json({ erro: "Quantidade de personagens escolhida inferior ao necessário (5)"})
+     } else if (personagem.length > 5) {
+       res.json({ erro: "Quantidade de personagens escolhida superior ao necessário (5)"}) 
+     } else if (personagem.length < 5) {
+     res.json({ erro: "Quantidade de personagens escolhida inferior ao necessário (5)"}) 
+     } else {
+        res.send(`<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+  <p><strong>Participante:</strong> ${nome}</p>
+  <p><strong>Email:</strong> ${email}</p>
+  <p><strong>Tipo de controle:</strong> ${controle}</p>
+  <p><strong>Personagens escolhidos:</strong></p>
+  <p>${personagem[0]}</p>
+  <p>${personagem[1]}</p>
+  <p>${personagem[2]}</p>
+  <p>${personagem[3]}</p>
+  <p>${personagem[4]}</p>
+</body>
+</html>`);
+     }
+    
+  }
+    
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
+
+module.exports = app
