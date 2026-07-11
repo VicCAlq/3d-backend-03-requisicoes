86fcc0180d7d16627dd70b57667ef8078500ce0d
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-07-11 18:59:46 -0300

    Update Atv01RespostaSimples.js

--- src/Atv01RespostaSimples.js
+++ src/Atv01RespostaSimples.js
@@ -27,3 +27,35 @@
   * Ao final deste arquivo, use "export default app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+  console.log(req)
+
+  if (!req.query) {
+    res.status(400).json({ error: erro.message });
+    return
+   
+res.send(`<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="color-scheme" content="light dark"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+  <title>Prazer em lhe conhecer</title>
+</head>
+<body>
+  <h1>Cadastro feito com sucesso!</h1>
+</body>
+</html>`)
+})
+
+export default app
+
+   
+  }
+
